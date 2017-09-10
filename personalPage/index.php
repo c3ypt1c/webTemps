2b73c1a845259c7298d3fac43c4ddc42c56f61e2
Lukasz <c3ypt1c@gmail.com>
2017-09-10 22:10:00 +0100

    Adding basic stuff

---
+++ personalPage/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html><?php include_once("config.php");?>
+<html>
+<head>
+
+	<title><?php echo $title?></title>
+</head>
+<body>
+
+<div id="nameSpace">
+	<p><?php echo $name;?></p>
+</div>
+
+<div id="aboutUs">
+
+
+</div>
+
+
+</body>
+</html>
